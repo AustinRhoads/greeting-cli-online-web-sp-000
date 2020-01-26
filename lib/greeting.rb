b6aa9b63772619ab0535f4088378d0f552709e38
Austin Rhoads <austinrrhoads@ymail.com>
2020-01-26 19:09:59 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,7 @@
 # code the #greeting method here!
+def squish!
+  gsub(/\A[[:space:]]+/,"")
+end
 def greeting (name)
-  puts "Hello #{name.squeeze("  ")}. It's nice to meet you."
+  puts "Hello #{name.squeeze("")}. It's nice to meet you."
 end
